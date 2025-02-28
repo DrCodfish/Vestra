725ea1f6042f5bb35bfa7ec745147ca0ab8ade35
DrCodfish <kiranrangoon0@proton.me>
2025-02-28 22:49:14 +0100

    Dekstop App Error?
    Downloaded dekstop app. Repos not showing. Debugging.

--- target/debug/deps/vestra-d819fa90ce5354f2.d
+++
@@ -1,5 +0,0 @@
-/home/kiranrangoon0/Downloads/vestra/target/debug/deps/vestra-d819fa90ce5354f2: src/main.rs
-
-/home/kiranrangoon0/Downloads/vestra/target/debug/deps/vestra-d819fa90ce5354f2.d: src/main.rs
-
-src/main.rs:
--- target/release/deps/vestra-7eba59022872f045.d
+++
@@ -1,5 +0,0 @@
-/home/kiranrangoon0/Downloads/vestra/target/release/deps/vestra-7eba59022872f045: src/main.rs
-
-/home/kiranrangoon0/Downloads/vestra/target/release/deps/vestra-7eba59022872f045.d: src/main.rs
-
-src/main.rs:
